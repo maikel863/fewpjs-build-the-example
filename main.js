70265a726e0a1825fc33926498100890c3cb4f2f
Maikel <maikel863@outlook.com>
2021-04-11 20:55:37 -0400

    Done with assignment

--- main.js
+++ main.js
@@ -2,9 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+
 // Your JavaScript code goes here!
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
